7896e09002d0e62176c90f9abeb7d7a5fced546a
Akash Kumar L <akash4657kumr@gmail.com>
2026-04-15 22:23:01 +0530

    checking if webhook gets requests

--- src/main/java/com/autotest/demo/DemoApplication.java
+++ src/main/java/com/autotest/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
- // checking for webhook actual
+ // checking for webhook act
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
