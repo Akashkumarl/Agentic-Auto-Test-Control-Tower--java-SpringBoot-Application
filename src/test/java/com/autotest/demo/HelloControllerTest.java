245a7590278303956e62a9008c3632958f7e1146
karthikeyan-ks <karthikeyanks3673@gmail.com>
2026-04-21 23:57:03 +0530

    unit test

--- src/test/java/com/autotest/demo/HelloControllerTest.java
+++ src/test/java/com/autotest/demo/HelloControllerTest.java
@@ -1,15 +1,17 @@
 package com.autotest.demo;
 
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-import java.util.Map;
-
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.bean.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
+
+import com.autotest.demo.service.MathService;
 
 @WebMvcTest(HelloController.class)
 public class HelloControllerTest {
@@ -17,10 +19,8 @@ public class HelloControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @Test
-    public void testHello() throws Exception {
-        mockMvc.perform(get("/api/v1/hello"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.message").value("Hello from AutoTest demo-app"));
-    }
+    @MockBean
+    private MathService mathService;
+
+    
 }
---
+++ src/test/java/com/autotest/demo/service/impl/MathServiceImplTest.java
@@ -0,0 +1,12 @@
+package com.autotest.demo.service.impl;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import org.junit.jupiter.api.Test;
+
+public class MathServiceImplTest {
+
+    private final MathServiceImpl mathService = new MathServiceImpl();
+
+    
+}
