19f1738760fa6ccdc7bea2c1425d7bbd368c517b
Akash Kumar L <akash4657kumr@gmail.com>
2026-04-23 00:11:57 +0530

    checking pushed

--- src/main/java/com/autotest/demo/DemoApplication.java
+++ src/main/java/com/autotest/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
- // checking testing testing final.....
+ // checking testing checking .
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
