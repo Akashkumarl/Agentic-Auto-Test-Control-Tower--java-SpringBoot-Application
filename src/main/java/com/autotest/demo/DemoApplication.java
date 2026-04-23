749ebb0bdbd7ffcdaf3a7e656cd1135b669580f0
Akash Kumar L <akash4657kumr@gmail.com>
2026-04-23 21:27:29 +0530

    checking for a push

--- src/main/java/com/autotest/demo/DemoApplication.java
+++ src/main/java/com/autotest/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
-// checking for a test1,3,4
+// checking for a test1,3,4 pushed
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
