152fd4aed5632f54116148ae5b0e2bed6a11ed17
Akash Kumar L <akash4657kumr@gmail.com>
2026-04-23 23:50:20 +0530

    automate and write the testcase through   AI 2

--- src/test/java/com/autotest/demo/DemoApplicationTest.java
+++ src/test/java/com/autotest/demo/DemoApplicationTest.java
@@ -6,8 +6,5 @@ import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class DemoApplicationTest {
 
-    @Test
-    void testMainMethodDoesNotThrow() {
-        assertDoesNotThrow(() -> DemoApplication.main(new String[0]));
-    }
+    //checking if Ai Automate through LLM (2)
 }
