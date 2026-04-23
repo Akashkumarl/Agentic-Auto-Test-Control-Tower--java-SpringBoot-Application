71e3d419563a5a53af5e1af328b8e7ad14c6af48
Akash Kumar L <akash4657kumr@gmail.com>
2026-04-23 20:56:01 +0530

    checking for a testK

--- src/main/java/com/autotest/demo/DemoApplication.java
+++ src/main/java/com/autotest/demo/DemoApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class DemoApplication {
- // checking testing checking .
+ // checking testing checking 1,2
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
