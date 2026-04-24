f9d9251dabfdb49f324f5679f3424a523e348098
Akash Kumar L <akash4657kumr@gmail.com>
2026-04-24 14:06:10 +0530

    automate and write the testcase through   AI 5

--- src/test/java/com/autotest/demo/DemoApplicationTest.java
+++ src/test/java/com/autotest/demo/DemoApplicationTest.java
@@ -5,9 +5,5 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
 
 class DemoApplicationTest {
-
-    @Test
-    void testMain() {
-        assertDoesNotThrow(() -> DemoApplication.main(new String[0]));
-    }
+    //checking agent tes
 }
